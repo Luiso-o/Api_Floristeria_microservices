caaefbef7362710ebb33f8b743025f74ee3ad0a6
luiso-o <cheportillo@gmail.com>
2023-09-27 16:40:37 +0200

    version 2.0

---
+++ src/test/java/ProyectoFloristeria/Api/Floristeria/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoFloristeria.Api.Floristeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
